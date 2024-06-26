4e05faa1eea4d34597b4bb8712efda68d8b6c413
KiranPrakash9470 <156503896+KiranPrakash9470@users.noreply.github.com>
2024-06-26 21:08:47 +0530

    Update EthAvaxModule1Project.sol

--- EthAvaxModule1Project.sol
+++ EthAvaxModule1Project.sol
@@ -1,31 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "hardhat/console.sol";
-
-contract ErrorHandling {
-    uint public age;
-
-    function setAge(uint _age) public{
-        age = _age;
+contract MyToken {
+    
+    constructor() {
+        owner = msg.sender; 
     }
 
-    function testAssert() public view{
-        assert(age >= 18 && age <= 80);
-        console.log("Congratulations! You are eligible to vote.");
+    // public variables here
+    string public name = "simple";
+    string public symbol = "SIMP";
+    uint public totalSupply = 0;
+    address public owner;
+
+    //emits Events
+    event Mint(address indexed to,  uint amount);
+    event Burn(address indexed from,  uint amount);
+    event Transfer(address indexed from, address indexed to, uint amount);
+
+    //errors
+    error InsufficientBalance(uint balance, uint withdrawAmount);
+
+    // mapping variable here
+    mapping(address => uint) public balances;
+
+    //Modifiers
+    modifier onlyOwner {
+      assert(msg.sender == owner);
+      _;
     }
 
-    function testRequire() public view{
-        require(age >= 18, "You must be at least 18 years old to be eligible to vote" );
-        console.log("Congratulations! You are eligible to vote.");
+    // mint function
+    function mint (address _address, uint _value) public onlyOwner{
+        totalSupply += _value;
+        balances[_address] += _value;
+        emit Mint(_address, _value);
     }
 
-    function testRevert() public view{
-        if (age < 18 || age > 80){
-            revert("You must be between 18 and 80 years old to be eligible to vote");
-        }
-        else{
-            console.log("Congratulations! You are eligible to vote.");
-        }
+    // burn function
+  function burn (address _address, uint _value) public onlyOwner{
+      if(balances[_address] <  _value){
+        revert InsufficientBalance({balance: balances[_address], withdrawAmount: _value});
+      }else{
+        totalSupply -= _value;
+        balances[_address] -= _value;
+        emit Burn(_address, _value); 
+      }
+        
+    }
+
+    function transfer (address _reciver, uint _value ) public{
+        require(balances[msg.sender] >=  _value , "Account balance must be greater then transfered value!");
+        balances[msg.sender] -= _value;
+        balances[_reciver] += _value;
+        emit Transfer(msg.sender, _reciver, _value);
     }
 }
